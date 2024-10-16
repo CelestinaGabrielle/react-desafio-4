35dcf9453baf124bc1a9ff19ceac88cf36956402
Gabrielle_CA <gabriellecelestinaa@gmail.com>
2024-10-16 17:11:48 -0300

    Botão desabilitado enquanto todas as informações não forem preenchidas corretamente

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const ButtonContainer = styled.button.attrs((props) => ({
+  type: "button",
+}))<React.ButtonHTMLAttributes<HTMLButtonElement>>`
+  background-color: ${({ disabled }) => (disabled ? "#5e636b" : "#81259d")}; 
+  width: 100%;
+  height: 42px; 
+  color: #fff; 
+  border: 1px solid #81259d; 
+  border-radius: 21px;
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)}; 
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  transition: background-color 0.3s, opacity 0.3s;
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  &:hover {
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 0.6)};
+    background-color: ${({ disabled }) => (disabled ? "#ccc" : "#472569")}; 
+  }
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:focus {
+    outline: none; // Remove o contorno padrão
+    box-shadow: 0 0 0 3px rgba(130, 37, 157, 0.5); 
+`;
